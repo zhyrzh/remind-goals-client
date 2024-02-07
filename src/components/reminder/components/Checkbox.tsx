8eedbd30f461c4d627d2c6c3f49f4762110e0026
zhyrzh <arongrhyz@gmail.com>
2024-02-07 16:06:40 +0800

    renamed directory for ReminderList

---
+++ src/components/reminder/components/Checkbox.tsx
@@ -0,0 +1,61 @@
+import { FC, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Checkbox } from "../../ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { IReminder } from "../ReminderItem";
+
+const CustomCheckbox: FC<{
+  goal: IReminder;
+  onSetGoalAsDone: (id: number) => void;
+}> = ({ goal, onSetGoalAsDone }) => {
+  const [showSetIsActiveDialog, setShowIsActiveDialog] =
+    useState<boolean>(false);
+  return (
+    <Dialog open={showSetIsActiveDialog} onOpenChange={setShowIsActiveDialog}>
+      <DialogTrigger asChild>
+        <Checkbox
+          className="transition-all disabled:cursor-auto"
+          checked={!goal.active}
+          disabled={!goal.active}
+        />
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Mark as done</DialogTitle>
+          <DialogDescription>
+            Are you sure you want this specific reminder to be marked as done?
+            You <b>WON'T</b> be able to retain it's status if you do so.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button
+            type="submit"
+            onClick={() => {
+              onSetGoalAsDone(goal.id);
+              setShowIsActiveDialog((prevVal) => !prevVal);
+            }}
+          >
+            Yes
+          </Button>
+          <Button
+            type="button"
+            className="bg-transparent text-slate-900 hover:bg-transparent"
+            onClick={() => setShowIsActiveDialog(false)}
+          >
+            No
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CustomCheckbox;
