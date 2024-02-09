e2ff4be38430af3fa9084e3705faa24759037450
zhyrzh <arongrhyz@gmail.com>
2024-02-09 19:17:59 +0800

    implemented live clock component

---
+++ src/components/clock/index.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import Moment from "react-moment";
+import { Card } from "../ui/card";
+
+const Clock = () => {
+  const [expectedTime, setExpectedTime] = useState(Date.now());
+  const [currentTime, setCurrentTime] = useState(Date.now());
+
+  useEffect(() => {
+    let timeout = setInterval(round, 1000);
+    function round() {
+      const drift = Date.now() - expectedTime;
+      setCurrentTime(Date.now());
+      setExpectedTime((prevExpected) => (prevExpected += 1000));
+      timeout = setTimeout(round, 1000 - drift);
+    }
+
+    return () => {
+      clearInterval(timeout);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Card className="col-start-1 col-end-4 h-[310px] flex justify-center items-center flex-col">
+      <h1 className="text-[69px] font-bold">
+        <Moment date={""} element={"time"} format={"MMM DD"}>
+          {currentTime}
+        </Moment>
+      </h1>
+      <h3 className="text-[18px] mb-12">
+        <span>
+          <Moment date={""} element={"time"} format={"ddd"}>
+            {currentTime}
+          </Moment>
+        </span>{" "}
+        -{" "}
+        <span>
+          <Moment date={""} element={"time"} format={"HH:mm:ss a"}>
+            {currentTime}
+          </Moment>
+        </span>
+      </h3>
+    </Card>
+  );
+};
+
+export default Clock;
