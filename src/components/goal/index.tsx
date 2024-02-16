7f925d3b629340cc3b3f33e65e44c855b2607541
zhyrzh <arongrhyz@gmail.com>
2024-02-16 12:55:03 +0800

    added custom goal card component

--- src/components/goal/index.tsx
+++ src/components/goal/index.tsx
@@ -1,27 +1,7 @@
-import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
-import { Card } from "../ui/card";
-import moment from "moment";
+import GoalItemCard from "./card.goal";
 
 const Goal = () => {
-  const [expectedTime, setExpectedTime] = useState(Date.now());
-  const [currentTime, setCurrentTime] = useState(Date.now());
-
-  useEffect(() => {
-    let timeout = setInterval(round, 1000);
-    function round() {
-      const drift = Date.now() - expectedTime;
-      setCurrentTime(Date.now());
-      setExpectedTime((prevExpected) => (prevExpected += 1000));
-      timeout = setTimeout(round, 1000 - drift);
-    }
-
-    return () => {
-      clearInterval(timeout);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
       <div className="flex flex-col mt-[35px]">
@@ -29,23 +9,7 @@ const Goal = () => {
           <h1 className="text-[24px] font-bold">Goals</h1>
           <Button>Add Goal</Button>
         </div>
-        <Card
-          className={
-            `bg-gradient-to-r from-indigo-500` +
-            ` from-${moment(currentTime).second()}% to-transparent to-${moment(
-              currentTime
-            ).second()}%`
-          }
-          style={{
-            background: `linear-gradient(90deg, rgba(0, 0, 0, 0.05) ${moment(
-              currentTime
-            ).second()}%, rgba(32, 32, 32, 0) ${moment(
-              currentTime
-            ).second()}%)`,
-          }}
-        >
-          <h1>{moment(currentTime).second()}</h1>
-        </Card>
+        <GoalItemCard />
       </div>
     </>
   );
