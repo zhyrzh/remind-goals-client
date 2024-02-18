de33859e28e96a466a5f4f71a0904bc1d3d5e956
zhyrzh <arongrhyz@gmail.com>
2024-02-18 12:57:57 +0800

    card implemntation of goal prgoress percentage

--- src/components/goal/card.goal.tsx
+++ src/components/goal/card.goal.tsx
@@ -1,14 +1,35 @@
+import { FC, useEffect, useState } from "react";
 import { Card } from "../ui/card";
+import { IGoal } from ".";
 
-const GoalItemCard = () => {
+const GoalItemCard: FC<{ goal: IGoal }> = ({ goal }) => {
+  const [progress, setProgress] = useState(
+    (
+      (goal.checklist.filter(({ isActive }) => isActive).length /
+        goal.checklist.length) *
+      100
+    ).toFixed(1)
+  );
+
+  useEffect(() => {
+    setProgress(
+      (
+        (goal.checklist.filter(({ isActive }) => isActive).length /
+          goal.checklist.length) *
+        100
+      ).toFixed(1)
+    );
+  }, [goal.checklist]);
+
   return (
     <Card
-      className="flex items-center p-2 transition-all duration-200 ease-in-out border-none first:mt-0 mt-2 cursor-pointer bg-neutral-300 hover:bg-neutral-300"
+      className="flex items-center justify-between p-2 transition-all duration-200 ease-in-out border-none first:mt-0 mt-2 cursor-pointer bg-neutral-300 hover:bg-neutral-300"
       style={{
-        background: `linear-gradient(90deg, rgb(212 212 212) ${5}%, rgb(245 245 245) ${5}%)`,
+        background: `linear-gradient(90deg, rgb(212 212 212) ${progress}%, rgb(245 245 245) ${progress}%)`,
       }}
     >
-      <h1>Card</h1>
+      <p>{goal.title}</p>
+      <p>{`${progress}% completed`}</p>
     </Card>
   );
 };
