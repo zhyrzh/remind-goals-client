a8abeb724cc1bbecaeb6346a358cd46247dfa52b
zhyrzh <arongrhyz@gmail.com>
2024-02-18 12:58:23 +0800

    added dummy data and onAddGoal functionality

--- src/components/goal/index.tsx
+++ src/components/goal/index.tsx
@@ -1,15 +1,80 @@
-import { Button } from "../ui/button";
+import { useState } from "react";
+import AddGoal from "./add.goal";
 import GoalItemCard from "./card.goal";
 
+export interface IGoalChecklist {
+  id: number;
+  title: string;
+  isActive: boolean;
+}
+export interface IGoal {
+  id: number;
+  title: string;
+  checklist: IGoalChecklist[];
+}
+
 const Goal = () => {
+  const [goals, setGoals] = useState<IGoal[]>([
+    {
+      id: 1,
+      title: "Build Dream PC",
+      checklist: [
+        {
+          id: 1,
+          title: "Gather funds",
+          isActive: true,
+        },
+        {
+          id: 2,
+          title: "Buy all components",
+          isActive: false,
+        },
+        {
+          id: 3,
+          title: "Build the pc",
+          isActive: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Buy new monitor",
+      checklist: [
+        {
+          id: 1,
+          title: "Gather funds",
+          isActive: true,
+        },
+        {
+          id: 2,
+          title: "Buy the monitor",
+          isActive: false,
+        },
+      ],
+    },
+  ]);
+
+  const onAddGoal = ({ title, checklist }: Omit<IGoal, "id">) => {
+    setGoals((prevGoals) => [
+      ...prevGoals,
+      {
+        id: prevGoals.length + 1,
+        title,
+        checklist,
+      },
+    ]);
+  };
+
   return (
     <>
       <div className="flex flex-col mt-[35px]">
         <div className="flex justify-between mb-[13px] items-center">
           <h1 className="text-[24px] font-bold">Goals</h1>
-          <Button>Add Goal</Button>
+          <AddGoal onAddGoal={onAddGoal} />
         </div>
-        <GoalItemCard />
+        {goals.map((goal) => (
+          <GoalItemCard goal={goal} key={goal.id} />
+        ))}
       </div>
     </>
   );
