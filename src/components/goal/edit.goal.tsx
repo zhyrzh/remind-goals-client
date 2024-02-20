d2dd7dd924544cd509d5e75276ec38c731510532
zhyrzh <arongrhyz@gmail.com>
2024-02-20 11:37:51 +0800

    added necessary states and functionalities

--- src/components/goal/edit.goal.tsx
+++ src/components/goal/edit.goal.tsx
@@ -1,8 +1,66 @@
-const EditGoal = () => {
+import { FC, useState } from "react";
+import Checklist from "../checklist";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { IGoalChecklist } from ".";
+
+interface IEditGoalProps {
+  goalTitle: string;
+  checklist: IGoalChecklist[];
+}
+
+const EditGoal: FC<IEditGoalProps> = ({ checklist, goalTitle }) => {
+  const [showEditGoalModal, setShowEditGoalModal] = useState<boolean>(false);
+  const [currentGoalTitle, setCurrentGoalTitle] = useState<string>(goalTitle);
+  const [currentChecklist, setCurrentChecklist] =
+    useState<IGoalChecklist[]>(checklist);
   return (
-    <div>
-      <h1>EditGoal</h1>
-    </div>
+    <Dialog open={showEditGoalModal} onOpenChange={setShowEditGoalModal}>
+      <DialogTrigger asChild>
+        <Button>Edit Goal</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit goal details</DialogTitle>
+        </DialogHeader>
+        <div>
+          <Label htmlFor="title">Title</Label>
+          <Input
+            type="text"
+            id="title"
+            name="title"
+            className="mt-2"
+            onChange={(e) => setCurrentGoalTitle(e.target.value)}
+            value={currentGoalTitle}
+          />
+        </div>
+        <Checklist
+          checklist={currentChecklist}
+          setChecklist={setCurrentChecklist}
+        />
+        <DialogFooter className="mt-12">
+          <Button type="submit" onClick={() => {}}>
+            Save Changes
+          </Button>
+          <Button
+            type="button"
+            className="bg-transparent text-slate-900 hover:bg-transparent"
+            onClick={() => setShowEditGoalModal(false)}
+          >
+            Cancel
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
