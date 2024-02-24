70bce5588f1e5bf95c5d0ed4b51ba2fb04b270a8
zhyrzh <arongrhyz@gmail.com>
2024-02-24 08:55:21 +0800

    implemented progress view and edit functionality

--- src/components/goal/card.goal.tsx
+++ src/components/goal/card.goal.tsx
@@ -1,20 +1,23 @@
 import { FC, useEffect, useState } from "react";
 import { Card } from "../ui/card";
 import { IGoal } from ".";
+import EditGoal from "./edit.goal";
 
-const GoalItemCard: FC<{ goal: IGoal }> = ({ goal }) => {
-  const [progress, setProgress] = useState(
-    (
-      (goal.checklist.filter(({ isActive }) => isActive).length /
-        goal.checklist.length) *
-      100
-    ).toFixed(1)
-  );
+interface IGoalItemCardProp {
+  goal: IGoal;
+  onEditGoal: (id: number, goal: Omit<IGoal, "id">) => void;
+}
+
+const GoalItemCard: FC<IGoalItemCardProp> = ({ goal, onEditGoal }) => {
+  // state declaration
+  const [progress, setProgress] = useState<string>("");
+
+  // methods declaration
 
   useEffect(() => {
     setProgress(
       (
-        (goal.checklist.filter(({ isActive }) => isActive).length /
+        (goal.checklist.filter(({ isActive }) => isActive !== true).length /
           goal.checklist.length) *
         100
       ).toFixed(1)
@@ -29,7 +32,15 @@ const GoalItemCard: FC<{ goal: IGoal }> = ({ goal }) => {
       }}
     >
       <p>{goal.title}</p>
-      <p>{`${progress}% completed`}</p>
+      <section className="flex items-center">
+        <p className="mr-2">{`${progress}% completed`}</p>
+        <EditGoal
+          checklist={goal.checklist}
+          goalTitle={goal.title}
+          onEditGoal={onEditGoal}
+          goalId={goal.id}
+        />
+      </section>
     </Card>
   );
 };
