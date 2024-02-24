e0b3cf7720049d3d40f99752664d4a20f6bfda55
zhyrzh <arongrhyz@gmail.com>
2024-02-24 08:57:49 +0800

    added toaster and custom goal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
 import Clock from "./components/clock";
 import Reminder from "./components/reminder";
+import Goal from "./components/goal";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
     <div className="max-w-[1400px] mx-auto ">
+      <Toaster />
       <div className="grid grid-cols-12 gap-[30px]">
         <Clock />
         <div className="col-start-4 col-end-13 transition-all">
           <Reminder />
         </div>
       </div>
-      <div className="flex flex-col mt-[35px]">
-        <div className="flex justify-between mb-[13px] items-center">
-          <h1 className="text-[24px] font-bold">Goals</h1>
-          <Button>Add Goal</Button>
-        </div>
-        {/* <ReminderList /> */}
-      </div>
+      <Goal />
     </div>
   );
 }
