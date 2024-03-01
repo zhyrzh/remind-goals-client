7e8c7f1c7c939e2d0de029826c5d945f6ea7a292
zhyrzh <arongrhyz@gmail.com>
2024-03-01 09:49:13 +0800

    removed (duplicate)

--- src/components/reminder/EditReminder.tsx
+++ src/components/reminder/EditReminder.tsx
@@ -13,6 +13,8 @@ import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Calendar } from "../ui/calendar";
 import moment from "moment";
 import { Button } from "../ui/button";
+import { Tabs, TabsContent } from "../ui/tabs";
+import useCurrTabDetails from "@/hooks/useCurrTabDetails";
 
 interface IEditReminder {
   onAddReminder: (value: { title: string; frequency: FrequencyEnum }) => void;
@@ -46,9 +48,9 @@ const radioItems = [
 ];
 
 const AddReminder: FC<IEditReminder> = ({ onAddReminder }) => {
+  // useState declarations
   const [showAddReminderModal, setShowAddReminderModal] =
     useState<boolean>(false);
-
   const [values, setValues] = useState<{
     title: string;
     frequency: FrequencyEnum;
@@ -57,11 +59,34 @@ const AddReminder: FC<IEditReminder> = ({ onAddReminder }) => {
     frequency: FrequencyEnum.once,
   });
 
+  // hooks declarations
+  const { currTabDetails, setCurrTabDetails } = useCurrTabDetails({
+    value: "main",
+    isCancel: false,
+    tabTitle: "",
+  });
+
+  // methods declaration
   const isDisabled = (current: Date) => {
     const curr = moment(current);
     // const customDate = moment().format("YYYY-MM-DD");
     // return curr && curr.isBefore(customDate);
     return curr.isBefore(moment());
+  };
+
+  const resetCurrTabDetails = () => {
+    setCurrTabDetails({
+      value: "main",
+      isCancel: false,
+      tabTitle: "",
+    });
+  };
+
+  const resetFields = () => {
+    setValues({
+      title: "",
+      frequency: FrequencyEnum.once,
+    });
   };
 
   return (
@@ -70,74 +95,124 @@ const AddReminder: FC<IEditReminder> = ({ onAddReminder }) => {
         <Button>Add Reminder</Button>
       </DialogTrigger>
       <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Add new reminder</DialogTitle>
-        </DialogHeader>
-        <div>
-          <Label htmlFor="title">Title</Label>
-          <Input
-            type="text"
-            id="title"
-            name="title"
-            className="mt-2"
-            value={values && values.title}
-            onChange={(e) =>
-              setValues((prevValues) => ({
-                ...prevValues,
-                title: e.target.value,
-              }))
-            }
-          />
-        </div>
-
-        <div className="flex transition-all">
-          <div>
-            <h1 className="font-medium text-sm mb-2">Frequncy</h1>
-            <RadioGroup
-              defaultValue={FrequencyEnum.once}
-              value={values.frequency}
-              onValueChange={(value: FrequencyEnum) =>
-                setValues((prevValues) => ({ ...prevValues, frequency: value }))
-              }
-            >
-              {radioItems.map(({ item }) => (
-                <div key={item} className="flex items-center space-x-2">
-                  <RadioGroupItem value={item} id={item} />
-                  <Label htmlFor={item}>
-                    {item.charAt(0).toUpperCase() + item.slice(1, item.length)}
-                  </Label>
-                </div>
-              ))}
-            </RadioGroup>
-          </div>
-          {values.frequency === FrequencyEnum.once && (
-            <div className="ml-12">
-              <h1 className="font-medium text-sm mb-2">Pick a date</h1>
-              <Calendar
-                disabled={isDisabled}
-                className="bg-slate-200 mx-auto"
+        <Tabs defaultValue="main" value={currTabDetails.value}>
+          <TabsContent value="main">
+            <DialogHeader>
+              <DialogTitle>Add new reminder</DialogTitle>
+            </DialogHeader>
+            <div>
+              <Label htmlFor="title">Title</Label>
+              <Input
+                type="text"
+                id="title"
+                name="title"
+                className="mt-2"
+                value={values && values.title}
+                onChange={(e) =>
+                  setValues((prevValues) => ({
+                    ...prevValues,
+                    title: e.target.value,
+                  }))
+                }
               />
             </div>
-          )}
-        </div>
-        <DialogFooter className="mt-12">
-          <Button
-            type="submit"
-            onClick={() => {
-              onAddReminder(values);
-              setShowAddReminderModal(false);
-            }}
-          >
-            Add Reminder
-          </Button>
-          <Button
-            type="button"
-            className="bg-transparent text-slate-900 hover:bg-transparent"
-            onClick={() => setShowAddReminderModal(false)}
-          >
-            Cancel
-          </Button>
-        </DialogFooter>
+
+            <div className="flex transition-all">
+              <div>
+                <h1 className="font-medium text-sm mb-2">Frequncy</h1>
+                <RadioGroup
+                  defaultValue={FrequencyEnum.once}
+                  value={values.frequency}
+                  onValueChange={(value: FrequencyEnum) =>
+                    setValues((prevValues) => ({
+                      ...prevValues,
+                      frequency: value,
+                    }))
+                  }
+                >
+                  {radioItems.map(({ item }) => (
+                    <div key={item} className="flex items-center space-x-2">
+                      <RadioGroupItem value={item} id={item} />
+                      <Label htmlFor={item}>
+                        {item.charAt(0).toUpperCase() +
+                          item.slice(1, item.length)}
+                      </Label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              </div>
+              {values.frequency === FrequencyEnum.once && (
+                <div className="ml-12">
+                  <h1 className="font-medium text-sm mb-2">Pick a date</h1>
+                  <Calendar
+                    disabled={isDisabled}
+                    className="bg-slate-200 mx-auto"
+                  />
+                </div>
+              )}
+            </div>
+            <DialogFooter className="mt-12">
+              <Button
+                type="submit"
+                onClick={() => {
+                  onAddReminder(values);
+                  setShowAddReminderModal(false);
+                }}
+              >
+                Add Reminder
+              </Button>
+              <Button
+                type="button"
+                className="bg-transparent text-slate-900 hover:bg-transparent"
+                onClick={() => {
+                  if (values.title && values.title.trim() !== "") {
+                    setCurrTabDetails({
+                      value: "confirmation",
+                      isCancel: true,
+                      tabTitle: "Are you sure you want to discard your changes",
+                    });
+                  } else {
+                    resetFields();
+                    setShowAddReminderModal(false);
+                  }
+                }}
+              >
+                Cancel
+              </Button>
+            </DialogFooter>
+          </TabsContent>
+          <TabsContent value="confirmation">
+            <DialogHeader>
+              <DialogTitle>{currTabDetails.tabTitle}</DialogTitle>
+            </DialogHeader>
+
+            <DialogFooter className="mt-12">
+              <Button
+                type="submit"
+                onClick={() => {
+                  if (!currTabDetails.isCancel) {
+                    console.log("triggered");
+                    onAddReminder(values);
+                  }
+                  resetCurrTabDetails();
+                  resetFields();
+                  setShowAddReminderModal(false);
+                }}
+              >
+                {!currTabDetails.isCancel ? "Confirm" : "Discard"}
+              </Button>
+              <Button
+                type="button"
+                className="bg-transparent text-slate-900 hover:bg-transparent"
+                onClick={() => {
+                  resetCurrTabDetails();
+                }}
+              >
+                Cancel
+              </Button>
+            </DialogFooter>
+          </TabsContent>
+        </Tabs>
       </DialogContent>
     </Dialog>
   );
