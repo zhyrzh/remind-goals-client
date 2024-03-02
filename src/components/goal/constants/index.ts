c145b8d719aea462d9668d7514b303f819c3c66a
zhyrzh <arongrhyz@gmail.com>
2024-03-02 11:47:56 +0800

    created a constants file for all constant data

---
+++ src/components/goal/constants/index.ts
@@ -0,0 +1,39 @@
+export const dummyGoals = [
+  {
+    id: 1,
+    title: "Build Dream PC",
+    checklist: [
+      {
+        id: 1,
+        title: "Gather funds",
+        isActive: true,
+      },
+      {
+        id: 2,
+        title: "Buy all components",
+        isActive: false,
+      },
+      {
+        id: 3,
+        title: "Build the pc",
+        isActive: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Buy new monitor",
+    checklist: [
+      {
+        id: 1,
+        title: "Gather funds",
+        isActive: true,
+      },
+      {
+        id: 2,
+        title: "Buy the monitor",
+        isActive: false,
+      },
+    ],
+  },
+];
