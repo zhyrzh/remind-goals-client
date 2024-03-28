08bed4a96050c4b7dbcfae674c94ccf7e1d7b5c0
zhyrzh <arongrhyz@gmail.com>
2024-03-29 02:31:09 +0800

    modified layout style and added goal context provider to goal component

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Clock from "./components/clock";
 import Reminder from "./components/reminder";
 import Goal from "./components/goal";
 import { Toaster } from "./components/ui/toaster";
+import GoalContextProvider from "./store/goal.context";
 
 function App() {
   return (
-    <div className="max-w-[1400px] mx-auto ">
+    <div className="max-w-[1400px] mx-auto px-2 pt-2">
       <Toaster />
       <div className="grid grid-cols-12 gap-[30px]">
         <Clock />
-        <div className="col-start-4 col-end-13 transition-all">
+        <div className="col-start-1 col-end-13 transition-all">
           <Reminder />
         </div>
       </div>
-      <Goal />
+      <GoalContextProvider>
+        <Goal />
+      </GoalContextProvider>
     </div>
   );
 }
