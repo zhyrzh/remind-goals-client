1623f499a05e18bf7f546217f621114c38eaac26
zhyrzh <arongrhyz@gmail.com>
2024-03-29 21:44:09 +0800

    resolves #8

--- src/store/checklist.context.tsx
+++ src/store/checklist.context.tsx
@@ -1,14 +1,43 @@
+import { IGoalChecklist } from "@/components/goal/types";
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { FC, createContext } from "react";
 
-interface IGoalChecklistContext {}
+interface IGoalChecklistContext {
+  getAllChecklistWithNoGoalIdQry: UseQueryResult<IGoalChecklist[]>;
+}
 
 export const ChecklistContext = createContext<IGoalChecklistContext>(
   null as unknown as IGoalChecklistContext
 );
 
 const ChecklistContextProvider: FC<{ children: any }> = ({ children }) => {
+  const getAllChecklistWithNoGoalIdQry = useQuery<IGoalChecklist[]>({
+    queryKey: ["goal-checklist", "get-all-with-no-goal-id"],
+    queryFn: async () => {
+      const res = await fetch(
+        "http://localhost:5000/goal-checklist/get-all-no-goal-id",
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFyb25ncmh5ekBnbWFpLmNvbSIsInN1YiI6ImFyb25ncmh5ekBnbWFpLmNvbSIsImlhdCI6MTcxMTY0NDk2NywiZXhwIjoxNzExNzMxMzY3fQ.KNfs6YelPFvii5kvwZXNPuD3YlgUn28PT3tq7wg28m0",
+          },
+        }
+      );
+
+      return await res.json();
+    },
+    placeholderData: [],
+    refetchOnWindowFocus: false,
+  });
+
   return (
-    <ChecklistContext.Provider value={{}}>{children}</ChecklistContext.Provider>
+    <ChecklistContext.Provider
+      value={{
+        getAllChecklistWithNoGoalIdQry,
+      }}
+    >
+      {children}
+    </ChecklistContext.Provider>
   );
 };
 
