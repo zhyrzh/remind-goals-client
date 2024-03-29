347676cacde10a4464124d5b547a60eb4b799400
zhyrzh <arongrhyz@gmail.com>
2024-03-29 22:15:14 +0800

    resolves #4

--- src/components/goal/types/index.d.ts
+++ src/components/goal/types/index.d.ts
@@ -2,6 +2,10 @@ export interface IGoalChecklist {
   id: number;
   title: string;
   isActive: boolean;
+  userId: string;
+  updatedAt: Date | null;
+  createdAt: Date;
+  goalId: number | null;
 }
 export interface IGoal {
   id: number;
