f10d5fca8fa7af102d06480ed92ab5b71cb3f1f6
zhyrzh <arongrhyz@gmail.com>
2024-03-29 19:03:33 +0800

    implemented dynamic prop type base on type property values

--- src/hooks/useCurrTabDetails.tsx
+++ src/hooks/useCurrTabDetails.tsx
@@ -1,10 +1,46 @@
+import { IGoal, IGoalChecklist } from "@/components/goal/types";
 import { useState } from "react";
 
-interface ICurrTabDetails {
+export type ICurrTabDetails = {
   value: string;
   tabTitle: string;
   isCancel: boolean;
-}
+} & (
+  | {
+      type: "checklist.toggle";
+      data: Pick<IGoalChecklist, "id" | "isActive">;
+    }
+  | {
+      type: "checklist.add.to-existing-goal";
+      data: {
+        goalId: number;
+        title: string;
+        isActive: boolean;
+      };
+    }
+  | {
+      type: "checklist.add";
+      data: Pick<IGoalChecklist, "title" | "isActive">;
+    }
+  | {
+      type: "checklist.edit.title";
+      data: Pick<IGoalChecklist, "id" | "title">;
+    }
+  | {
+      type: "checklist.delete";
+      data: Pick<IGoalChecklist, "id">;
+    }
+  | {
+      type: "goal.edit.title";
+      data: Pick<IGoal, "title" | "id">;
+    }
+  | {
+      type: "goal.add";
+    }
+  | {
+      type: undefined;
+    }
+);
 
 const useCurrTabDetails = (value: ICurrTabDetails) => {
   const [currTabDetails, setCurrTabDetails] = useState<ICurrTabDetails>(value);
