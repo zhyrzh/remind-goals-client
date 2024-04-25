19485d85d562e84b483d3c577ff2ee2e9a09286a
zhyrzh <arongrhyz@gmail.com>
2024-04-26 02:26:42 +0800

    modified empty goals conditons message and added redirection base on user status

--- src/components/goal/index.tsx
+++ src/components/goal/index.tsx
@@ -2,11 +2,23 @@ import { useContext } from "react";
 import AddGoal from "./add.goal";
 import GoalItemCard from "./card.goal";
 import { GoalContext } from "@/store/goal.context";
+import { useNavigate } from "react-router-dom";
 
 /* Function component START */
 const Goal = () => {
+  const navigate = useNavigate();
+
   // context declarations
   const goalCtx = useContext(GoalContext)?.getAllGoalsQry;
+
+  if (goalCtx.isLoading) {
+    return <h1>Loading</h1>;
+  }
+
+  if (goalCtx.isError && goalCtx.error.res.status === 401) {
+    localStorage.removeItem("remind-goals-ath-tkn");
+    navigate("/login");
+  }
 
   return (
     <>
@@ -15,11 +27,13 @@ const Goal = () => {
           <h1 className="text-[24px] font-bold">Goals</h1>
           <AddGoal />
         </div>
-        {goalCtx?.data
-          ? goalCtx.data?.map((goal) => (
-              <GoalItemCard goal={goal} key={goal.id} />
-            ))
-          : null}
+        {goalCtx?.data && goalCtx?.data.length >= 1 ? (
+          goalCtx.data?.map((goal) => (
+            <GoalItemCard goal={goal} key={goal.id} />
+          ))
+        ) : (
+          <p className="text-center">No goas created.</p>
+        )}
       </div>
     </>
   );
