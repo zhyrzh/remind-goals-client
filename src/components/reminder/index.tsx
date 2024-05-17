7bedd98c4750a186d4561e6e656bec528c4af097
zhyrzh <arongrhyz@gmail.com>
2024-05-17 08:10:44 +0800

    added redirection when error occurs

--- src/components/reminder/index.tsx
+++ src/components/reminder/index.tsx
@@ -2,10 +2,21 @@ import { useContext } from "react";
 import AddReminder from "./add.reminder";
 import ReminderList from "./list.reminder";
 import { ReminderContext } from "@/store/reminder.context";
+import { useNavigate } from "react-router-dom";
 
 /* Function component START */
 const Reminder = () => {
+  const navigate = useNavigate();
   const remindersQry = useContext(ReminderContext).getAllRemindersQry;
+
+  if (remindersQry.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (remindersQry.isError && remindersQry.error.res.status === 401) {
+    localStorage.removeItem("remind-goals-ath-tkn");
+    navigate("/login");
+  }
 
   return (
     <>
