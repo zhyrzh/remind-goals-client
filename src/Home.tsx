c3ff908860509a883a141b7557bf171cc43bac0a
zhyrzh <arongrhyz@gmail.com>
2024-05-23 13:23:16 +0800

    added all data to be fetched and redirection logic

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,10 +3,37 @@ import Clock from "./components/clock";
 import Reminder from "./components/reminder";
 import Goal from "./components/goal";
 import { Toaster } from "./components/ui/toaster";
-import GoalContextProvider from "./store/goal.context";
-import { ReminderContextProvder } from "./store/reminder.context";
+import GoalContextProvider, { GoalContext } from "./store/goal.context";
+import {
+  ReminderContext,
+  ReminderContextProvder,
+} from "./store/reminder.context";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  const getAllRemindersQry = useContext(ReminderContext)?.getAllRemindersQry;
+  const getAllGoalsQry = useContext(GoalContext)?.getAllGoalsQry;
+
+  useEffect(() => {
+    const userDetails = JSON.parse(
+      localStorage.getItem("remind-goals-ath-tkn")!
+    );
+
+    if (userDetails && userDetails.profile === null) {
+      navigate("/setup-profile");
+    }
+  }, []);
+
+  if (getAllGoalsQry?.isLoading || getAllRemindersQry?.isLoading) {
+    return <h1>Loading</h1>;
+  }
+
+  if (getAllGoalsQry?.isError && getAllGoalsQry.error.res.status === 401) {
+    localStorage.removeItem("remind-goals-ath-tkn");
+  }
+
   return (
     <div className="max-w-[1400px] mx-auto px-2 pt-2">
       <Toaster />
