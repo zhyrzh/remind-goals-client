456e616950421a4c272b82e486801358a07a49c9
zhyrzh <arongrhyz@gmail.com>
2024-05-25 19:49:09 +0800

    imported routes that makes up the entire app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import "./App.css";
-import Clock from "./components/clock";
-import Reminder from "./components/reminder";
-import Goal from "./components/goal";
-import { Toaster } from "./components/ui/toaster";
-import GoalContextProvider from "./store/goal.context";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 function App() {
-  return (
-    <div className="max-w-[1400px] mx-auto px-2 pt-2">
-      <Toaster />
-      <div className="grid grid-cols-12 gap-[30px]">
-        <Clock />
-        <div className="col-start-1 col-end-13 transition-all">
-          <Reminder />
-        </div>
-      </div>
-      <GoalContextProvider>
-        <Goal />
-      </GoalContextProvider>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
