41235dc25d6fc8321ed5c06fb28cd4381557bbbb
zhyrzh <arongrhyz@gmail.com>
2024-05-29 21:00:39 +0800

    implemented useFetchRequest reusability hook for fetching api data

--- src/hooks/useGoalChecklistAPIRequest.tsx
+++ src/hooks/useGoalChecklistAPIRequest.tsx
@@ -1,151 +1,70 @@
 import { IGoalChecklist } from "@/components/goal/types";
-import { FetchError } from "@/utils/error";
 import { MutationFunction, QueryFunctionContext } from "@tanstack/react-query";
+import useFetchRequest from "./useFetchREquest";
 
 export const useGoalChecklistAPIRequest = () => {
-  const userDetails = JSON.parse(localStorage.getItem("remind-goals-ath-tkn")!);
-  const token = userDetails?.access_token ? userDetails?.access_token : "";
-  const headers: HeadersInit = {
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
-  };
-
   const baseUrl = "http://localhost:5000/goal-checklist";
 
   const getAllChecklistWithNoGoalIdReq = async (
     _context: QueryFunctionContext
-  ) => {
-    const res = await fetch(`${baseUrl}/get-all-no-goal-id`, {
-      headers,
-    });
-
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  ) => useFetchRequest(`${baseUrl}/get-all-no-goal-id`, "GET")();
 
   const getAllChecklistByGoalIdReq = async ({
-    queryKey,
-  }: QueryFunctionContext) => {
-    const [_primKey, _subKey, goalId] = queryKey;
-    const res = await fetch(`${baseUrl}/get-all-by-goal-id/${goalId}`, {
-      headers,
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+    queryKey: [_primKey, _subKey, goalId],
+  }: QueryFunctionContext) =>
+    useFetchRequest(`${baseUrl}/get-all-by-goal-id/${goalId}`, "GET")();
 
   const addGoalChklistItmReq: MutationFunction<
     IGoalChecklist,
     { title: string; isActive: boolean }
-  > = async (values) => {
-    const res = await fetch("http://localhost:5000/goal-checklist", {
-      headers,
-      method: "POST",
-      body: JSON.stringify(values),
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  > = async (values) =>
+    useFetchRequest("http://localhost:5000/goal-checklist", "POST", values)();
 
   const addGoalChecklistItmToExistingGoalReq: MutationFunction<
     IGoalChecklist,
     { title: string; isActive: boolean; goalId: number }
-  > = async (values) => {
-    const res = await fetch(
+  > = async (values) =>
+    useFetchRequest(
       `http://localhost:5000/goal-checklist/to-existing-goal/${values.goalId}`,
-      {
-        headers,
-        method: "POST",
-        body: JSON.stringify({
-          title: values.title,
-          isActive: values.isActive,
-        }),
-      }
-    );
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+      "POST",
+      JSON.stringify({
+        title: values.title,
+        isActive: values.isActive,
+      })
+    )();
 
   const deleteAllNoGoalIdReq: MutationFunction<
     { count: number },
     undefined
-  > = async () => {
-    const res = await fetch(
+  > = async () =>
+    useFetchRequest(
       "http://localhost:5000/goal-checklist/all/no-goal-id",
-      {
-        headers,
-        method: "DELETE",
-      }
-    );
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+      "DELETE"
+    )();
 
   const toggleChecklistItmStatusReq: MutationFunction<
     IGoalChecklist,
     { checklistItmId: number; isActive: boolean }
-  > = async ({ checklistItmId, isActive }) => {
-    const res = await fetch(
+  > = async ({ checklistItmId, isActive }) =>
+    useFetchRequest(
       `http://localhost:5000/goal-checklist/toggle-is-active/${checklistItmId}/${isActive}`,
-      {
-        headers,
-        method: "PUT",
-      }
-    );
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+      "PUT"
+    )();
 
   const deleteSpecificChecklistItmReq: MutationFunction<
     IGoalChecklist,
     { id: number }
-  > = async ({ id }) => {
-    const res = await fetch(`http://localhost:5000/goal-checklist/${id}`, {
-      headers,
-      method: "DELETE",
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  > = async ({ id }) =>
+    useFetchRequest(`http://localhost:5000/goal-checklist/${id}`, "DELETE")();
 
   const editChecklistItmTitleReq: MutationFunction<
     IGoalChecklist,
     { id: number; title: string }
-  > = async ({ id, title }) => {
-    const res = await fetch(
+  > = async ({ id, title }) =>
+    useFetchRequest(
       `http://localhost:5000/goal-checklist/edit-title/${id}/${title}`,
-      {
-        headers,
-        method: "PUT",
-      }
-    );
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+      "PUT"
+    )();
 
   return {
     getAllChecklistWithNoGoalIdReq,
