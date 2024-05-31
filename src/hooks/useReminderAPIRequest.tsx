9fe0852547d9f60d918dc6d477ad1b91a7f677ae
zhyrzh <arongrhyz@gmail.com>
2024-05-31 18:57:07 +0800

    implemented useFetchRequest reusability hook for fetching api data

--- src/hooks/useReminderAPIRequest.tsx
+++ src/hooks/useReminderAPIRequest.tsx
@@ -1,75 +1,30 @@
 import { FrequencyEnum } from "@/components/reminder/constants";
 import { IReminder } from "@/components/reminder/types";
-import { FetchError } from "@/utils/error";
 import { MutationFunction, QueryFunction } from "@tanstack/react-query";
+import useFetchRequest from "./useFetchREquest";
 
 export const useReminderAPIrequest = () => {
-  const userDetails = JSON.parse(localStorage.getItem("remind-goals-ath-tkn")!);
-  const token = userDetails?.access_token ? userDetails?.access_token : "";
-  const headers: HeadersInit = {
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
-  };
-
   const baseUrl = "http://localhost:5000/reminder";
 
-  const getAllRemindersReq: QueryFunction<IReminder[]> = async () => {
-    const res = await fetch(`${baseUrl}`, {
-      method: "GET",
-      headers,
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  const getAllRemindersReq: QueryFunction<IReminder[]> = async () =>
+    useFetchRequest(`${baseUrl}`, "GET")();
 
   const createReminderReq: MutationFunction<
     IReminder,
     { content: string; frequency: FrequencyEnum; isActive: boolean }
-  > = async (values) => {
-    const res = await fetch(`${baseUrl}`, {
-      method: "POST",
-      headers,
-      body: JSON.stringify(values),
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  > = async (values) =>
+    useFetchRequest(`${baseUrl}`, "POST", JSON.stringify(values))();
 
   const toggleIsActiveReq: MutationFunction<
     IReminder,
     { id: number; isActive: boolean }
-  > = async ({ id, isActive }) => {
-    const res = await fetch(`${baseUrl}/${id}/${isActive}`, {
-      method: "PUT",
-      headers,
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  > = async ({ id, isActive }) =>
+    useFetchRequest(`${baseUrl}/${id}/${isActive}`, "PUT")();
 
   const deleteReminderItemReq: MutationFunction<
     { count: number },
     { id: number }
-  > = async ({ id }) => {
-    const res = await fetch(`${baseUrl}/${id}`, {
-      method: "DELETE",
-      headers,
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  > = async ({ id }) => useFetchRequest(`${baseUrl}/${id}`, "DELETE")();
 
   const editReminderDetailsReq: MutationFunction<
     IReminder,
@@ -79,18 +34,12 @@ export const useReminderAPIrequest = () => {
       frequency: FrequencyEnum;
       reminderStartDate: Date;
     }
-  > = async ({ id, content, frequency, reminderStartDate }) => {
-    const res = await fetch(`${baseUrl}/details/${id}`, {
-      method: "PUT",
-      headers,
-      body: JSON.stringify({ content, frequency, reminderStartDate }),
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  > = async ({ id, content, frequency, reminderStartDate }) =>
+    useFetchRequest(
+      `${baseUrl}/details/${id}`,
+      "PUT",
+      JSON.stringify({ content, frequency, reminderStartDate })
+    )();
 
   return {
     getAllRemindersReq,
