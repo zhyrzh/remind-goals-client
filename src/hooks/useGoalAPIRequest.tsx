019a0001430cd1768acb2df90a4c402da90d26c1
zhyrzh <arongrhyz@gmail.com>
2024-06-01 19:26:12 +0800

    implemented useFetchRequest reusability hook for fetching api data

--- src/hooks/useGoalAPIRequest.tsx
+++ src/hooks/useGoalAPIRequest.tsx
@@ -1,75 +1,35 @@
 import { IGoal, IGoalChecklist } from "@/components/goal/types";
-import { FetchError } from "@/utils/error";
 import { MutationFunction, QueryFunction } from "@tanstack/react-query";
+import useFetchRequest from "./useFetchREquest";
 
 export const useGoalAPIRequest = () => {
-  const userDetails = JSON.parse(localStorage.getItem("remind-goals-ath-tkn")!);
-  const token = userDetails?.access_token ? userDetails?.access_token : "";
-  const headers: HeadersInit = {
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
-  };
-
   const baseUrl = "http://localhost:5000/goals";
 
-  const getAllGoalsReq: QueryFunction<IGoal[]> = async () => {
-    const res = await fetch(`${baseUrl}`, {
-      headers,
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  const getAllGoalsReq: QueryFunction<IGoal[]> = async () =>
+    useFetchRequest(`${baseUrl}`, "GET")();
 
-  const getSpecificGoalReq: QueryFunction<IGoal> = async ({ queryKey }) => {
-    const [_primKey, id] = queryKey;
-    const res = await fetch(`http://localhost:5000/goals/specific/${id}`, {
-      headers,
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-    return await res.json();
-  };
+  const getSpecificGoalReq: QueryFunction<IGoal> = async ({
+    queryKey: [_primKey, id],
+  }) => useFetchRequest(`${baseUrl}/specific/${id}`, "GET")();
 
   const addGoalReq: MutationFunction<
     IGoal,
     { title: string; checklist: Pick<IGoalChecklist, "id">[] }
-  > = async ({ title, checklist }) => {
-    const res = await fetch("http://localhost:5000/goals", {
-      method: "POST",
-      body: JSON.stringify({
+  > = async ({ title, checklist }) =>
+    useFetchRequest(
+      `${baseUrl}`,
+      "POST",
+      JSON.stringify({
         title,
         checklist,
-      }),
-      headers,
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+      })
+    )();
 
   const editGoalTitleReq: MutationFunction<
     IGoal,
     { title: string; id: number }
-  > = async ({ id, title }) => {
-    const res = await fetch(`http://localhost:5000/goals/${id}`, {
-      method: "PUT",
-      body: JSON.stringify({ title }),
-      headers,
-    });
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new FetchError(res);
-    }
-  };
+  > = async ({ id, title }) =>
+    useFetchRequest(`${baseUrl}/${id}`, "PUT", JSON.stringify({ title }))();
 
   return { getAllGoalsReq, getSpecificGoalReq, addGoalReq, editGoalTitleReq };
 };
