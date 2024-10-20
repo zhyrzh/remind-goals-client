3141c77f031cbc359f7f9b42de92054284e594b8
Rhyz <arongrhyz@gmail.com>
2024-10-21 00:25:30 +0800

    added logout handler method

--- src/store/auth.context.tsx
+++ src/store/auth.context.tsx
@@ -1,15 +1,33 @@
-import { createContext, FC } from "react";
+import { createContext, FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 
-interface IAuthContext {}
+interface IAuthContext {
+  isLoggedIn: boolean;
+  onLogoutHandler: () => void;
+}
 
 export const AuthContext = createContext<IAuthContext>(
   null as unknown as IAuthContext
 );
 
 export const AuthContextProvider: FC<{ children: any }> = ({ children }) => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const onLogoutHandler = () => {
+    localStorage.removeItem("remind-goals-ath-tkn");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
+
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider
+      value={{
+        onLogoutHandler,
+        isLoggedIn,
+      }}
+    >
       <Toaster />
       {children}
     </AuthContext.Provider>
