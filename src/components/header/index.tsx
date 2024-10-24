9c8c32383560d6f1de8f758a3171ba771529696e
Rhyz <arongrhyz@gmail.com>
2024-10-25 00:03:06 +0800

    created header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { AuthContext } from "@/store/auth.context";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  const authCtx = useContext(AuthContext);
+
+  return (
+    <header className="h-16 w-full bg-primary text-primary-foreground">
+      <div className="max-w-[1400px] h-full px-2 mx-auto flex items-center">
+        <h1 className="text-3xl font-bold">RemindGoals</h1>
+        {authCtx.isLoggedIn ? (
+          <p
+            className="ml-auto hover:cursor-pointer"
+            onClick={() => {
+              authCtx?.onLogoutHandler();
+            }}
+          >
+            LOGOUT
+          </p>
+        ) : (
+          <>
+            <Button className="ml-auto hover:cursor-pointer bg-primary-foreground text-primary mr-3 hover:bg-primary-foreground">
+              LOGIN
+            </Button>
+            <p
+              className="hover:cursor-pointer"
+              onClick={() => {
+                authCtx?.onLogoutHandler();
+              }}
+            >
+              SIGNUP
+            </p>
+          </>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
