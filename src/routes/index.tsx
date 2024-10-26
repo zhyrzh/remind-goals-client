bfdef996eb6a9f428f0ebe700c7ba7955637a3ab
Rhyz <arongrhyz@gmail.com>
2024-10-27 01:59:01 +0800

    added auth context provider as parent element

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,27 +3,46 @@ import Home from "@/Home";
 import Login from "@/components/auth/Login";
 import SetupProfile from "@/components/auth/SetupProfile";
 import Signup from "@/components/auth/Signup";
+import Header from "@/components/header";
+import { AuthContextProvider } from "@/store/auth.context";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <AuthContextProvider>
+        <Home />
+      </AuthContextProvider>
+    ),
     errorElement: <Error />,
   },
   {
     path: "/login",
-    element: <Login />,
+    element: (
+      <AuthContextProvider>
+        <Login />
+      </AuthContextProvider>
+    ),
     errorElement: <Error />,
   },
   {
     path: "/signup",
-    element: <Signup />,
+    element: (
+      <AuthContextProvider>
+        <Signup />
+      </AuthContextProvider>
+    ),
     errorElement: <Error />,
   },
   {
     path: "/setup-profile",
-    element: <SetupProfile />,
+    element: (
+      <AuthContextProvider>
+        <Header />
+        <SetupProfile />
+      </AuthContextProvider>
+    ),
     errorElement: <Error />,
   },
 ]);
