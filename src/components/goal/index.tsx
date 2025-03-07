b86371bf364eb0ae61210c18167412a2bd557270
Rhyz <arongrhyz@gmail.com>
2025-03-07 21:32:28 +0800

    moved all error handling on useeffect when a query has an error status

--- src/components/goal/index.tsx
+++ src/components/goal/index.tsx
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AddGoal from "./add.goal";
 import GoalItemCard from "./card.goal";
 import { GoalContext } from "@/store/goal.context";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../ui/spinner";
+import Cookies from "js-cookie";
 
 /* Function component START */
 const Goal = () => {
@@ -11,6 +12,14 @@ const Goal = () => {
 
   // context declarations
   const goals = useContext(GoalContext)?.getAllGoalsQry;
+
+  useEffect(() => {
+    if (goals.isError && goals.error.res.status === 401) {
+      localStorage.removeItem("remind-goals-ath-tkn");
+      Cookies.remove("my-key");
+      navigate("/login");
+    }
+  }, [goals.isError]);
 
   if (goals.isPending) {
     return <Spinner />;
